d6a6b45f542b7b3c4a0ecb7efdc003a7746e3afd
Ben Pack <benpack007@gmail.com>
2021-05-18 12:24:12 -0700

    Init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv(),
+    new CompressionPlugin({
+      filename: 'bundle.js.gz',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8,
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+};
